b57e9439952594118e0073c3a6978d7704369f4e
borjafg <borjafg_95@hotmail.com>
2016-02-17 17:30:40 +0100

    Empezando a añadir datos para hacer casos de prueba

--- src/main/java/voterAcess/Application.java
+++ src/main/java/voterAcess/Application.java
@@ -22,8 +22,21 @@ public class Application {
 		public CommandLineRunner init(VoterRepository repository) {
 			return (args) -> { 
 				// save a voter
-				repository.save(new Voter("Jack", "980151","pepe@gmail.com",1,"1"));
-//				repository.save(new Voter("Jack", "980151","pepe@gmail.com",1,"1"));
+				repository.save(new Voter("Jack", "980151", "jk@gmail.com", 1, "1"));
+				repository.save(new Voter("Jose", "730438", "josep@hotmail.com", 1, "2"));
+				repository.save(new Voter("Paula", "730438", "PaGu@terra.com", 1, "3"));
+				repository.save(new Voter("Alfonso", "730438", "alf@hotmail.com", 1, "4"));
+				
+				repository.save(new Voter("Irene", "730438", "GlezIr@hotmail.com", 2, "1"));
+				
+				repository.save(new Voter("Fernando", "730438", "fcano@terra.com", 3, "2"));
+				repository.save(new Voter("Vinuesa", "730438", "vinu@hotmail.com", 3, "3"));
+				repository.save(new Voter("Sotorrío", "730438", "soto@hotmail.com", 3, "4"));
+				
+				repository.save(new Voter("Hugo", "730438", "hg@gmail.com", 4, "5"));
+				repository.save(new Voter("Diana", "730438", "diana23@hotmail.com", 4, "6"));
+				repository.save(new Voter("Javier", "730438", "javiG_6@gmail.com", 4, "7"));
+				repository.save(new Voter("Luis", "730438", "luisValdés@terra.com", 4, "8"));
 				
 				/*
 				// fetch all customers
